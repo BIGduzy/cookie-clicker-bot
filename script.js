a4a5797f733153c3a17211b76062df11502d9b14
BIGduzy <nick-b@live.nl>
2019-02-21 12:25:48 +0100

    Better css prefix and replaced tabs with spaces

--- script.js
+++ script.js
@@ -9,36 +9,36 @@
 // ==/UserScript==
 
 class Data {
-	constructor() {
-		this.money = 0;
-		this.moneyPerSecond = 0;
-		this.data = [];
-	}
+    constructor() {
+        this.money = 0;
+        this.moneyPerSecond = 0;
+        this.data = [];
+    }
 
-	setMoney(amount) {
-		this.money = amount;
-	}
+    setMoney(amount) {
+        this.money = amount;
+    }
 
-	setMoneyPerSecond(amount) {
-		this.moneyPerSecond = amount;
-	}
+    setMoneyPerSecond(amount) {
+        this.moneyPerSecond = amount;
+    }
 
     calculateProductData(id) {
-		const data = this.data[id];
+        const data = this.data[id];
         const percentage = Math.min(99.9, this.money / data.productPrice * 100); // 0 - 99 %
-		const timeLeft = Math.max(0, parseInt((data.productPrice - this.money) / this.moneyPerSecond, 10)); // >= 0
-		const incomePerMoney = Math.round(data.mps / (data.productPrice || 1) * 100000);
+        const timeLeft = Math.max(0, parseInt((data.productPrice - this.money) / this.moneyPerSecond, 10)); // >= 0
+        const incomePerMoney = Math.round(data.mps / (data.productPrice || 1) * 100000);
 
         return {percentage, timeLeft, incomePerMoney};
     }
 
-	getProductData(id) {
-		return {...this.data[id], ...this.calculateProductData(id)};
-	}
+    getProductData(id) {
+        return {...this.data[id], ...this.calculateProductData(id)};
+    }
 
-	setProductData(id, data) {
-		this.data[id] = {...this.data[id], ...data}
-	}
+    setProductData(id, data) {
+        this.data[id] = {...this.data[id], ...data}
+    }
 
     getMostEfficientProduct() {
         // TODO: Sort products based on efficiency, so that we can buy second most efficent when wait time is short
@@ -65,7 +65,7 @@ function getProducts() {
 }
 
 function getProductId(product) {
-	return parseInt(product.id.split("product")[1]);
+    return parseInt(product.id.split("product")[1]);
 }
 
 function getProductById(id) {
@@ -77,126 +77,126 @@ function stringToInt(str) {
 }
 
 function secondsToTime(sec) {
-	if (sec / 86400 > 1) {
-		return `${Math.round(sec/86400, 10)}d`;
-	} else if (sec / 3600 > 1) { // Hours
-		return `${Math.round(sec/3600, 10)}h`;
-	} else if (sec / 60 > 1) { // Minute
-		return `${Math.round(sec/60, 10)}m`;
-	} else { // Sec
-		return `${sec}s`;
-	}
+    if (sec / 86400 > 1) {
+        return `${Math.round(sec/86400, 10)}d`;
+    } else if (sec / 3600 > 1) { // Hours
+        return `${Math.round(sec/3600, 10)}h`;
+    } else if (sec / 60 > 1) { // Minute
+        return `${Math.round(sec/60, 10)}m`;
+    } else { // Sec
+        return `${sec}s`;
+    }
 }
 
 function getProductInfo(id) {
-	// Open tooltip via game
-	Game.tooltip.dynamic = 1;
-	Game.tooltip.draw(this, () => { return Game.ObjectsById[id].tooltip(); }, 'store');
-	Game.tooltip.wobble();
+    // Open tooltip via game
+    Game.tooltip.dynamic = 1;
+    Game.tooltip.draw(this, () => { return Game.ObjectsById[id].tooltip(); }, 'store');
+    Game.tooltip.wobble();
 
-	const tooltip = document.getElementById('tooltip');
-	const data = tooltip.getElementsByTagName('b');
-	const mpsElement = data[0] || document.createElement('b');
-	const mps = stringToInt(mpsElement.innerText);
+    const tooltip = document.getElementById('tooltip');
+    const data = tooltip.getElementsByTagName('b');
+    const mpsElement = data[0] || document.createElement('b');
+    const mps = stringToInt(mpsElement.innerText);
 
-	// Hide tooltip
-	Game.tooltip.hide();
+    // Hide tooltip
+    Game.tooltip.hide();
 
-	return mps;
+    return mps;
 }
 
 function render() {
-	const productsElements = getProducts();
+    const productsElements = getProducts();
 
-	for (const productElement of productsElements) {
-		const productId = getProductId(productElement);
-		const productData = data.getProductData(productId);
-		// Progress bar
-        const progressbar = productElement.querySelector("#nok-progressbar") || createProgressbar(productElement);
+    for (const productElement of productsElements) {
+        const productId = getProductId(productElement);
+        const productData = data.getProductData(productId);
+        // Progress bar
+        const progressbar = productElement.querySelector(`#${preFix}progressbar`) || createProgressbar(productElement);
         progressbar.style.width = `${productData.percentage}%`;
 
-		// Timer
-		const timer = progressbar.querySelector("#nok-timer");
-		timer.innerText = secondsToTime(productData.timeLeft);
+        // Timer
+        const timer = progressbar.querySelector(`#${preFix}timer`);
+        timer.innerText = secondsToTime(productData.timeLeft);
 
-		// Mps
-		const incomePerMoney = productData.incomePerMoney;
-		if (!isNaN(incomePerMoney)) {
-			const mpsElement = progressbar.querySelector("#nok-mps").innerText = incomePerMoney;
-		}
-	}
+        // Mps
+        const incomePerMoney = productData.incomePerMoney;
+        if (!isNaN(incomePerMoney)) {
+            const mpsElement = progressbar.querySelector(`#${preFix}mps`).innerText = incomePerMoney;
+        }
+    }
 }
 
 function updateProductData() {
-	const productsElements = getProducts();
-	for (const productElement of productsElements) {
-		const productId = getProductId(productElement);
-		const mps = getProductInfo(productId);
+    const productsElements = getProducts();
+    for (const productElement of productsElements) {
+        const productId = getProductId(productElement);
+        const mps = getProductInfo(productId);
 
-		data.setProductData(productId, {
+        data.setProductData(productId, {
             id: productId,
-			productPrice: stringToInt(productElement.querySelector(".content .price").innerText),
-			mps,
-		});
+            productPrice: stringToInt(productElement.querySelector(".content .price").innerText),
+            mps,
+        });
     }
-	console.log(data.data);
+    console.log(data.data);
 }
 
 function update() {
-	// Update Money
+    // Update Money
     const moneyString = document.getElementById('cookies').innerText.split("coo");
-	data.setMoney(stringToInt(moneyString[0]));
-	data.setMoneyPerSecond(stringToInt(moneyString[1].split(": ")[1]));
+    data.setMoney(stringToInt(moneyString[0]));
+    data.setMoneyPerSecond(stringToInt(moneyString[1].split(": ")[1]));
 
-	// Update product data if we have no data, else we do it on click
-	if(data.data.length === 0) { updateProductData(data); }
+    // Update product data if we have no data, else we do it on click
+    if(data.data.length === 0) { updateProductData(data); }
 
-	// Calculate most efficient product
+    // Calculate most efficient product
     mostEfficientProductElement = getProductById(data.getMostEfficientProduct().id);
 
-	// Buy most efficient product
-	if (mostEfficientProductElement.className.includes("enabled")) {
-		mostEfficientProductElement.click();
-	}
+    // Buy most efficient product
+    if (mostEfficientProductElement.className.includes("enabled")) {
+        mostEfficientProductElement.click();
+    }
 
-	// Render
-	render(data);
+    // Render
+    render(data);
 }
 
 function onClick() {
-	// Hacky way to make sure our onClick is fired after the sites onClick
-	window.setTimeout(() => {
-		updateProductData();
-	}, 10);
+    // Hacky way to make sure our onClick is fired after the sites onClick
+    window.setTimeout(() => {
+        updateProductData();
+    }, 10);
 }
 
 function createProgressbar(domNode) {
     const progressbar = document.createElement('div');
-    progressbar.id = "nok-progressbar";
+    progressbar.id = `${preFix}progressbar`;
     Object.assign(progressbar.style, {
         backgroundColor: "rgba(20, 200, 20, 0.2)",
         width: "99%",
         height: "100%",
     });
 
-	const timer = document.createElement('span');
-	timer.id = "nok-timer";
-	Object.assign(timer.style, {
-		left: "75%",
-		top: "65%",
-		position: "absolute"
-	})
+    const timer = document.createElement('span');
+    timer.id = `${preFix}timer`;
+    Object.assign(timer.style, {
+        left: "75%",
+        top: "65%",
+        position: "absolute"
+    })
 
-	const mps = document.createElement('span');
-	mps.id = "nok-mps";
-	Object.assign(mps.style, {
-		left: "75%",
-		top: "15%",
-		position: "absolute"
-	})
+    const mps = document.createElement('span');
+    mps.id = `${preFix}mps`;
+    Object.assign(mps.style, {
+        left: "75%",
+        top: "15%",
+        position: "absolute"
+    })
 
-	progressbar.appendChild(timer);
-	progressbar.appendChild(mps);
+    progressbar.appendChild(timer);
+    progressbar.appendChild(mps);
     domNode.appendChild(progressbar);
 
     return progressbar;
@@ -204,18 +204,19 @@ function createProgressbar(domNode) {
 
 // Create empty global data object
 const data = new Data();
+const preFix = "idle-bot-";
 
 {
-	document.addEventListener('click', onClick);
-	// TODO: Wait for coockie clicker to load, (since its not loaded when onLoad is called)
-	// Since cookie clicker is not done loading when window.load is called we just wait a second d;)
-	window.setTimeout(() => {
-		console.log(data);
-		// Update every 100 ms
-		const updateInterval = window.setInterval(update, 100);
-		const goldenCookieInterval = window.setInterval(() => {
-			const goldenCookie = document.querySelector(".shimmer");
-			if (goldenCookie) { goldenCookie.click(); }
-		}, 1000);
-	}, 1000);
+    document.addEventListener('click', onClick);
+    // TODO: Wait for coockie clicker to load, (since its not loaded when onLoad is called)
+    // Since cookie clicker is not done loading when window.load is called we just wait a second d;)
+    window.setTimeout(() => {
+        console.log(data);
+        // Update every 100 ms
+    	const updateInterval = window.setInterval(update, 100);
+    	const goldenCookieInterval = window.setInterval(() => {
+            const goldenCookie = document.querySelector(".shimmer");
+            if (goldenCookie) { goldenCookie.click(); }
+        }, 1000);
+    }, 1000);
 }
